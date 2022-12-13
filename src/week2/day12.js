f76a2bc2de539087115ec1f903624fe1c698999f
Francois Perron <fperron@gmail.com>
2022-12-12 20:30:10 -0500

    Day 12

---
+++ src/week2/day12.js
@@ -0,0 +1,52 @@
+export const fewestStepsPart1 = input => {
+  const { start, end, map } = parseHeightmap(input)
+  const foundIt = (start, end) => end.x === start.x && end.y === start.y
+
+  return fewestSteps(start, end, map, foundIt)
+}
+
+export const fewestStepsPart2 = input => {
+  const { start, end, map } = parseHeightmap(input)
+  const foundIt = (start) => start.height === 'a'.charCodeAt(0)
+
+  return fewestSteps(start, end, map, foundIt)
+}
+
+export const fewestSteps = (start, end, map, foundIt) => {
+  const toVisit = [end]
+  while (toVisit.length !== 0) {
+    const point = toVisit.shift()
+    for (const adjacent of adjacentsTo(point, map)) {
+      if (foundIt(adjacent, start)) {
+        return point.distance + 1
+      } else {
+        adjacent.visited = true
+        adjacent.distance = point.distance + 1
+        toVisit.push(adjacent)
+      }
+    }
+  }
+}
+
+export const adjacentsTo = (point, map) =>
+  [{ x: point.x - 1, y: point.y }, { x: point.x + 1, y: point.y }, { x: point.x, y: point.y - 1 }, { x: point.x, y: point.y + 1 }]
+    .map(a => map.find(point => pointsAreEqual(point, a)))
+    .filter(a => a !== undefined)
+    .filter(a => !a.visited)
+    .filter(a => (point.height - a.height) < 2)
+
+const pointsAreEqual = (p1, p2) => p1.x === p2.x && p1.y === p2.y
+
+export const parseHeightmap = input => {
+  const map = input
+    .split('\n')
+    .flatMap((row, y) => row.split('').map((height, x) => ({ x: x, y: y, height: height.charCodeAt(0), distance: 0 })))
+
+  const start = map.find(p => p.height === 'S'.charCodeAt(0))
+  start.height = 'a'.charCodeAt(0)
+
+  const end = map.find(p => p.height === 'E'.charCodeAt(0))
+  end.height = 'z'.charCodeAt(0)
+
+  return { start, end, map }
+}
---
+++ src/week2/day12.spec.js
@@ -0,0 +1,74 @@
+import { dailyInput } from '../dailyInput.js'
+import { adjacentsTo, fewestStepsPart1, fewestStepsPart2, parseHeightmap } from './day12.js'
+
+describe('Day 12: Hill Climbing Algorithm', () => {
+  const example =
+    'Sabqponm\n' +
+    'abcryxxl\n' +
+    'accszExk\n' +
+    'acctuvwj\n' +
+    'abdefghi'
+
+  let input
+  beforeEach(async () => input = await dailyInput(12))
+
+  describe('Part 1: What is the fewest steps required to move from your current position to the location that should get the best signal?', () => {
+    let start, end, map
+    beforeEach(() => {
+      ({ start, end, map } = parseHeightmap(example))
+    })
+
+    it('parses each points', () => {
+      expect(map.length).to.equal(40)
+      expect(map[39]).to.deep.equal({ x: 7, y: 4, height: 'i'.charCodeAt(0), distance: 0 })
+    })
+
+    it('finds starting point', () => {
+      expect(start).to.deep.equal({ x: 0, y: 0, height: 'a'.charCodeAt(0), distance: 0 })
+    })
+
+    it('finds end point', () => {
+      expect(end).to.deep.equal({ x: 5, y: 2, height: 'z'.charCodeAt(0), distance: 0 })
+    })
+
+    it('finds climbing adjacent points to start', () => {
+      const adjacentsToStart = adjacentsTo(start, map)
+      expect(adjacentsToStart.length).to.equal(2)
+      expect(adjacentsToStart[0]).to.deep.equal({ x: 1, y: 0, height: 'a'.charCodeAt(0), distance: 0 })
+      expect(adjacentsToStart[1]).to.deep.equal({ x: 0, y: 1, height: 'a'.charCodeAt(0), distance: 0 })
+    })
+
+    it('finds climbing adjacent points to end', () => {
+      const adjacentsToEnd = adjacentsTo(end, map)
+      expect(adjacentsToEnd.length).to.equal(1)
+      expect(adjacentsToEnd[0]).to.deep.equal({ x: 4, y: 2, height: 'z'.charCodeAt(0), distance: 0 })
+    })
+
+    it('does not climb a point already visited', () => {
+      map.find(p => p.x === 4 && p.y === 2).visited = true
+
+      const adjacentsToEnd = adjacentsTo(end, map)
+      expect(adjacentsToEnd.length).to.equal(0)
+    })
+
+    it('solves example', () => {
+      expect(fewestStepsPart1(example)).to.equal(31)
+    })
+
+    it('solves it', () => {
+      expect(fewestStepsPart1(input)).to.equal(383)
+    })
+  })
+
+  describe('Part 2: What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?', () => {
+    it('solves example', () => {
+      expect(fewestStepsPart2(example)).to.equal(29)
+    })
+
+    it('solves it', () => {
+      expect(fewestStepsPart2(input)).to.equal(377)
+    })
+  })
+})
+
+
